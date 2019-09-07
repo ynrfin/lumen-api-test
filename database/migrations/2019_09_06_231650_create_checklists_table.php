9fc6885d1b901c9d1190854072d9444bf678dfa5
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-09-07 09:50:12 +0700

    add migration to create table checklist

---
+++ database/migrations/2019_09_06_231650_create_checklists_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateChecklistsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('checklists', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('object_domain');
+            $table->text('description');
+            $table->boolean('is_completed')->nullable();
+            $table->dateTime('completed_at')->nullable();
+            $table->datetime('due')->nullable();
+            $table->integer('urgency')->default(0);
+            $table->string('updated_by')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('checklists');
+    }
+}
