63f8d4abc2b6f0974c0ca38706a2d98c918ce6f9
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-09-08 11:58:35 +0700

    add Patch handler to checklist controller

--- app/Http/Controllers/ChecklistController.php
+++ app/Http/Controllers/ChecklistController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Checklist;
+use App\Helpers\Formatter;
+use App\User;
 use App\Http\Transformers\ChecklistTransformer;
 use App\Http\Transformers\ChecklistSerializer;
 use App\Http\Transformers\CustomPaginator;
@@ -12,6 +14,10 @@ use League\Fractal\Pagination\IlluminatePaginatorAdapter;
 use League\Fractal\Resource\Collection;
 use League\Fractal\Resource\Item;
 use DB;
+use Illuminate\Validation\Rule;
+use Validator;
+use App\Helpers\Validation as ValidationHelper;
+use Illuminate\Support\Carbon;
 
 class ChecklistController extends Controller
 {
@@ -48,12 +54,62 @@ class ChecklistController extends Controller
         $res = new Item($checklist, new ChecklistTransformer());
         $res =  fractal($checklist, new ChecklistTransformer());
         //dd($res);
-        $checklistResponse = fractal()
-            ->item($checklist)
-            ->transformWith(new ChecklistTransformer())
-            ->serializeWith(new ChecklistSerializer())
-            ->toArray();
+        //$checklistResponse = fractal()
+        //    ->item($checklist)
+        //    ->transformWith(new ChecklistTransformer())
+        //    ->serializeWith(new ChecklistSerializer())
+        //    ->toArray();
 
         return response()->json($res->toArray());
+    }
+
+    /**
+     * update resource by id
+     *
+     * @return void
+     */
+    public function patch(Request $request, $id)
+    {
+        $validator = Validator::make($request->input(),[
+            'data' => 'required',
+            'data.type' => 'required|in:checklists',
+            'data.id' => 'required|integer',
+            'data.attributes' => 'required',
+            'data.attributes.object_domain' => 'required',
+            'data.attributes.object_id' => 'required|string',
+            'data.attributes.description' => 'required|string',
+            'data.attributes.is_completed' => 'boolean',
+            'data.attributes.created_at' => 'required|string',
+            'data.attributes.updated_by' => 'string',
+            'data.attributes.due' =>[function($attribute, $value, $fail){
+                if(null == $value || Carbon::hasFormat($value, "Y:m:d H:i:s")){
+                }else{
+                    $fail($attribute . "not null and wrong format");
+                }
+            }],
+            'data.links.self' => 'required|string',
+        ]);
+
+        if($validator->fails()){
+            return response()->json($validator->errors(), 422);
+        }
+
+        $checklist = Checklist::findOrFail($id);
+        $attributes = $request->input("data.attributes");
+
+        if(array_key_exists('created_at', $attributes)){
+            unset($attributes['created_at']);
+        }
+        if(array_key_exists('updated_at', $attributes)){
+            unset($attributes['updated_at']);
+        }
+
+        unset($attributes["last_update_by"]);
+        $attributes['updated_by'] = $request->user()->email;
+        $checklist->update($attributes);
+
+        $res =  fractal($checklist, new ChecklistTransformer());
+        return response()->json($res);
+
     }
 }
