8e62789f62ee7895da4ae5ada7fc59664a2ab193
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-09-08 10:54:02 +0700

    use database transaction on test"

--- tests/ChecklistTest.php
+++ tests/ChecklistTest.php
@@ -6,7 +6,7 @@ use App\User;
 
 class ChecklistTest extends TestCase
 {
-    use DatabaseMigrations;
+    use DatabaseTransactions;
     protected $baseUrl = '/api/v1/checklists';
 
     protected $checklistStructure = [
@@ -44,4 +44,86 @@ class ChecklistTest extends TestCase
                          ->seeStatusCode(200)
                      ->seeJsonStructure($this->checklistStructure);
     }
+
+    /**
+     * testing for no record returns 404
+     *
+     * @return void
+     */
+    public function testGetNonExistentResourceReturn404()
+    {
+        factory(App\Checklist::class, 5)->create();
+        $user = Factory(App\User::class)->create();
+
+        $response = $this->actingAs($user)
+            ->get('/51')
+            ->seeStatusCode(404)
+            ->seeJsonEquals(['status' => 404, 'error' => 'Not Found']);
+    }
+
+    /**
+     * make sure that patch will take validation first, 
+     * then search for record
+     *
+     * @return void
+     */
+    public function testPatchNonExistentRecordWithInvalidDataReturns422()
+    {
+        $user = Factory(App\User::class)->create();
+        factory(App\Checklist::class, 5)->create();
+        $response = $this->actingAs($user)
+            ->patch('/40')
+            ->seeStatusCode(422);
+    }
+
+    /**
+     * testing the validation is done first even with valid resource id
+     *
+     * @return void
+     */
+    public function testPatchExistingRecordWithInvalidDataReturns422()
+    {
+        factory(App\Checklist::class, 5)->create();
+        $user = Factory(App\User::class)->create();
+
+        $this->actingAs($user)
+             ->patch('/4')
+             ->seeStatusCode(422);
+    }
+
+    /**
+     * validated message will patch the resource
+     *
+     * @return json
+     */
+    public function testPatchWithMinimumRequiredFieldReturns200()
+    {
+        factory(App\Checklist::class, 5)->create();
+        $user = Factory(App\User::class)->create();
+        $attributes = [
+            "object_domain" => "contact",
+            "object_id" => "1",
+            "description" => "Need to verify this guy house.",
+            "is_completed" => false,
+            "completed_at" => null,
+        ];
+
+        $dirtyAttribute = $attributes;
+        $dirtyAttribute['created_at'] ="2018-01-25T07:50:14+00:00" ;
+
+        $this->actingAs($user)
+            ->patch('/2', [
+            "data" => [
+                'id' => 2,
+                'type' => 'checklists', 
+                'attributes' => $dirtyAttribute,
+                'links' => [
+                    "self" => 'some-links'
+                ]
+            ]
+        ])->seeStatusCode(200)
+        ->seeInDatabase($attributes);
+
+    }
+
 }
