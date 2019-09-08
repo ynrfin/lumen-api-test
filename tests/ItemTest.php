b9293cbc4f3133602ec7302f774027d4b2a24524
Yanuar Arifin <3326885+ynrfin@users.noreply.github.com>
2019-09-08 21:28:59 +0700

    add test with full payload

--- tests/ItemTest.php
+++ tests/ItemTest.php
@@ -84,5 +84,44 @@ class ItemTest extends TestCase
         $this->post('/3/items')->seeStatusCode(401);
     }
     
+    /**
+     * test full valid params
+     *
+     * @return void
+     */
+    public function testCreateFullParams()
+    {
+        factory(App\Checklist::class, 5)->create();
+        $user = Factory(App\User::class)->create();
+        $attributes = [
+            "description" => 'some descripton',
+            "due" => date("Y-m-d H:i:s"),
+            "urgency" => 2,
+            "assignee_id"  => 23,
+            "task_id" => 3,
+            "updated_by" => 3,
+            "is_completed" => true,
+        ];
+
+        $fullPayload = [
+            "data" => [
+                "attributes" => $attributes
+            ]
+        ];
+
+        $resultAttribute = $attributes;
+        unset($resultAttribute['assignee_id']);
+        unset($resultAttribute['task_id']);
+
+        $resultLink = ['self' => route('checklists.showOne', ['id' => 3])];
+
+        $this->actingAs($user)
+            ->post('/3/items', $fullPayload)
+            ->seeInDatabase('items', $attributes)
+            ->seeJsonStructure($this->itemResponseStructure)
+            ->seeJsonContains($resultAttribute)
+            ->seeJsonContains($resultLink)
+            ->seeStatusCode(200);
+    }
 
 }
